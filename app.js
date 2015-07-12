6158fb17fa22d30665036a344997eb7865f51573
NastiaS <anastasiasergienko@gmail.com>
2015-07-12 15:44:34 -0400

    last commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('it does not see the deploy file');
+  res.send('last time');
 });
  
 app.listen(process.env.PORT || 5000);
--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../app.js');
  
 describe('GET /', function() {
   it('respond with Heyo, jenkins is working!', function(done) {
-    request(app).get('/').expect('it does not see the deploy file', done);
+    request(app).get('/').expect('last time', done);
   });
 });
