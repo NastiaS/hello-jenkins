cc0679b8f4162bd23b57101e019eacd6eba08567
NastiaS <anastasiasergienko@gmail.com>
2015-07-12 15:27:32 -0400

    a commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('Heyo, jenkins is working!');
+  res.send('Heyo, jenkins is working! I think');
 });
  
 app.listen(process.env.PORT || 5000);
