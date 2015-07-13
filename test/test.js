3b4d6e3d26b61e39a0332939328f7ec5ca3f1367
NastiaS <anastasiasergienko@gmail.com>
2015-07-13 09:13:12 -0400

    jenkins commit

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../app.js');
  
 describe('GET /', function() {
   it('respond with Heyo, jenkins is working!', function(done) {
-    request(app).get('/').expect('the very last time', done);
+    request(app).get('/').expect('Morning', done);
   });
 });
